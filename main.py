12676e09f049f136209718622f81218facfffff2
MACX <help@google.com>
2021-10-27 11:40:06 -0400

    4

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from paramiko import SSHClient, AutoAddPolicy
-from righ import print, pretty, inspect
+from rich import print, pretty, inspect
+import paramiko
+
 pretty.install()
+k = paramiko.RSAKey.from_private_key_file("/Users/x/.ssh/newk.pem")
 
 client = SSHClient()
-client.load_host_keys('~/.ssh/newk.pem')
+client.load_host_keys('/Users/x/.ssh/known_hosts')
+client.load_system_host_keys()
+client.set_missing_host_key_policy(AutoAddPolicy())
+client.connect('54.147.174.78', username='ubuntu', pkey= k, port=19999)
 
-client.connect('54.147.174.78', username='ubuntu')
+stdin, stdout, stderr = client.exec_command('uname -a')
+
+if stdout.channel.recv_exit_status() == 0:
+	print(f'STDOUT: {stdout.read().decode("utf8")}')
+else:
+	print(f'STDOUT: {stdout.read().decode("utf8")}')
+
+stdin.close()
+stdout.close()
+stderr.close()
+
+#CLOSE SSH CLIENT
+client.close
